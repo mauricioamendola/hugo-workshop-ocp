4839d4ce1943604bbfbbe5894b09e465970ac928
Mauricio Amendola <mauricio@linux.com.uy>
2020-12-03 17:47:33 -0300

    changed hugo base URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /usr/local/src && /usr/local/bin/hugo server --port=1313 --bind=0.0.0.0
+cd /usr/local/src && /usr/local/bin/hugo server --port=1313 --bind=0.0.0.0 --baseURL=http://workshop-openshift.apps.cap01.corp.ute.com.uy --disableFastRender --appendPort=false
